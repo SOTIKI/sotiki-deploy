c94768a7fef4f516e8e31bab92186925e2af70d7
Suzume <suzumealpha.2@gmail.com>
2026-03-02 01:09:47 +0500

    fix: Minor changes

--- scripts/add-shebang.mjs
+++
@@ -1,5 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs'
-
-const f = new URL('../dist/cli/index.js', import.meta.url).pathname
-const c = readFileSync(f, 'utf8')
-if (!c.startsWith('#!')) writeFileSync(f, '#!/usr/bin/env node\n' + c)
